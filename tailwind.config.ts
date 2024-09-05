372346f5ad7c76e7f70d6f30a4d3ed6e1a8e2b85
Aleksander Evensen <ahse03@gmail.com>
2024-09-05 17:42:04 +0200

    Grid pattern tailwind utility type

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,39 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
+// @ts-expect-error No types available
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
+import svgToDataUri from "mini-svg-data-uri";
+
+const addVariablesForColors = plugin(function ({ addBase, theme }) {
+    const allColors = flattenColorPalette(theme("colors"));
+    const newVars = Object.fromEntries(
+        Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+    );
+
+    addBase({
+        ":root": newVars,
+    });
+});
 
 export default {
-    darkMode: ["class"],
+    darkMode: "class",
     content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {
+            animation: {
+                moveUp: "moveUp 1.4s ease forwards",
+                appear: "appear 1s 1s forwards",
+            },
+            keyframes: {
+                moveUp: {
+                    "0%": { transform: "translateY(5%)", opacity: "0" },
+                    "100%": { transform: "translateY(0%)", opacity: "1" },
+                },
+                appear: {
+                    from: { opacity: "0" },
+                    to: { opacity: "1" },
+                },
+            },
             borderRadius: {
                 lg: "var(--radius)",
                 md: "calc(var(--radius) - 2px)",
@@ -54,5 +83,23 @@ export default {
             },
         },
     },
-    plugins: [require("tailwindcss-animate")],
+    plugins: [
+        require("tailwindcss-animate"),
+        addVariablesForColors,
+        plugin(({ matchUtilities, theme }) => {
+            matchUtilities(
+                {
+                    "bg-grid": (value) => ({
+                        backgroundImage: `url("${svgToDataUri(
+                            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+                        )}")`,
+                    }),
+                },
+                {
+                    values: flattenColorPalette(theme("backgroundColor")),
+                    type: "color",
+                }
+            );
+        }),
+    ],
 } satisfies Config;
