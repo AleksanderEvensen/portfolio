263cae0cf9a15ca60afd26dedd848a04c3c2bf8d
Aleksander Evensen <ahse03@gmail.com>
2024-10-10 00:13:09 +0200

    Made site responsive and finished tech and about section

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,17 @@
 import { Link, NavLink } from "@remix-run/react";
 import { RemixNavLinkProps } from "@remix-run/react/dist/components";
 import { BookCopy, Home } from "lucide-react";
+import { useState } from "react";
 import { cn } from "~/lib/utils";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Button } from "./ui/button";
 
 export default function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
         <header className="sticky top-0 z-50 w-full border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 grid place-items-center">
-            <div className="container flex h-20 max-w-screen-2xl items-center justify-between px-20">
+            <div className="container hidden md:flex h-20 max-w-screen-2xl items-center justify-between px-20">
                 <Link to="/" className="mx-4 flex items-center space-x-2">
                     <img
                         src="/logo.svg"
@@ -33,6 +38,72 @@ export default function Navbar() {
                         <span className="text-lg font-dm-sans">Projects</span>
                     </NavElement>
                 </nav>
+            </div>
+
+            <div className="container md:hidden flex h-20 max-w-screen-2xl items-center justify-between px-5">
+                <Link to="/" className="mx-4 flex items-center space-x-2">
+                    <img
+                        src="/logo.svg"
+                        alt="logo"
+                        className="aspect-square h-10 text-white"
+                    />
+                    <span className="text-lg font-space-mono">Aleksander</span>
+                </Link>
+                <Sheet open={isMenuOpen} onOpenChange={setIsMenuOpen}>
+                    <SheetTrigger asChild>
+                        <Button variant={"ghost"}>
+                            <svg
+                                strokeWidth="1.5"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                            >
+                                <path
+                                    d="M3 5H11"
+                                    stroke="currentColor"
+                                    strokeWidth="1.5"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                ></path>
+                                <path
+                                    d="M3 12H16"
+                                    stroke="currentColor"
+                                    strokeWidth="1.5"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                ></path>
+                                <path
+                                    d="M3 19H21"
+                                    stroke="currentColor"
+                                    strokeWidth="1.5"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                ></path>
+                            </svg>
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent side="right" className="flex flex-col gap-5">
+                        <NavElement
+                            to={"/"}
+                            className="[&.active-url]:underline underline-offset-8"
+                            onClick={() => setIsMenuOpen(false)}
+                        >
+                            <Home />
+                            <span className="text-lg font-dm-sans">Home</span>
+                        </NavElement>
+                        <NavElement
+                            to={"/projects"}
+                            className="[&.active-url]:underline underline-offset-8"
+                            onClick={() => setIsMenuOpen(false)}
+                        >
+                            <BookCopy />
+                            <span className="text-lg font-dm-sans">
+                                Projects
+                            </span>
+                        </NavElement>
+                    </SheetContent>
+                </Sheet>
             </div>
         </header>
     );
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,48 @@
 import type { MetaFunction } from "@vercel/remix";
-import { ArrowDown, Github, Linkedin } from "lucide-react";
+import {
+    ArrowDown,
+    Code,
+    Database,
+    Globe,
+    Library,
+    Smartphone,
+} from "lucide-react";
+
+import {
+    SiGithub as Github,
+    SiLinkedin as Linkedin,
+    SiAndroid,
+    SiCss3,
+    SiDotnet,
+    SiDrizzle,
+    SiFlutter,
+    SiGo,
+    SiHtml5,
+    SiJavascript,
+    SiLua,
+    SiMongodb,
+    SiMysql,
+    SiNextdotjs,
+    SiPostgresql,
+    SiPrisma,
+    SiPython,
+    SiReact,
+    SiRemix,
+    SiRust,
+    SiSass,
+    SiSurrealdb,
+    SiSvelte,
+    SiTailwindcss,
+    SiTypescript,
+    SiVuedotjs,
+    SiZig,
+} from "@icons-pack/react-simple-icons";
+
+import React, { createRef, useCallback } from "react";
 import Blob from "~/components/Blob";
+import { Button } from "~/components/ui/button";
+import { Card, CardContent, CardHeader } from "~/components/ui/card";
+import { Separator } from "~/components/ui/separator";
 
 export const meta: MetaFunction = () => {
     return [
@@ -14,6 +56,14 @@ export const handle = {
 };
 
 export default function Index() {
+    const aboutRef = createRef<HTMLDivElement>();
+
+    const gotoAbout = useCallback(() => {
+        aboutRef.current?.scrollIntoView({
+            behavior: "smooth",
+        });
+    }, [aboutRef]);
+
     return (
         <>
             {/* Main Section */}
@@ -50,49 +100,262 @@ export default function Index() {
                         </a>
                     </div>
 
-                    <a
-                        href="#aboutMe"
-                        className="rounded-xl bg-white text-black flex gap-2 py-2 px-4 hover:bg-white/50 transition-colors"
+                    <Button
+                        onClick={gotoAbout}
+                        className="rounded-xl flex gap-2"
                     >
                         <ArrowDown /> About Me
-                    </a>
+                    </Button>
                 </div>
             </div>
 
-            {/* About Me Section */}
-            <div
-                id="aboutMe"
-                className="w-full grid place-items-center scroll-mt-20"
-            >
-                <div className="grid grid-cols-2 gap-6 p-20">
-                    <div className="">
-                        <h2 className="text-2xl mb-2">About Me</h2>
-                        <p>
-                            Hi, my name is Aleksander. I am a software developer
-                            from Moss, Norway. I have since a young age been
-                            very interested in technology and computers. I
-                            started programming when I was 11 and have since
-                            then been learning new technologies and programming
-                            languages. I am currently studying Computer Science
-                            at NTNU in Trondheim, Norway.
-                        </p>
-                    </div>
-                    <div className="relative">
-                        <img
-                            className="rounded-xl aspect-square object-cover"
-                            src="/images/aerial_photo_moss.jpg"
-                            alt="moss"
-                        />
+            <div className="w-full flex flex-col items-center">
+                <div
+                    ref={aboutRef}
+                    className="container max-w-screen-2xl place-items-center scroll-mt-20"
+                >
+                    <div className="grid md:grid-cols-2 gap-6 p-5 md:p-20 place-items-center">
+                        <div>
+                            <h2 className="text-2xl font-semibold mb-2">
+                                About Me
+                            </h2>
+                            <p>
+                                Hello, I'm Aleksander, a software developer from
+                                Moss, Norway. I've been passionate about
+                                technology and computers from an early age,
+                                starting to code at the age of 11. My journey in
+                                web development began at 14, and over the past
+                                few years, I've expanded my expertise to focus
+                                on backend and systems development. During this
+                                time, I've learned languages like Rust, Go, and
+                                Zig. In addition to my interests in software, I
+                                have a passion for space exploration and rocket
+                                technology. A dream of mine would be to work in
+                                both fields. Combining my love for software and
+                                complex systems with space technology. I'm
+                                currently working towards a bachelor's degree in
+                                Computer Engineering at NTNU in Trondheim,
+                                Norway.
+                            </p>
+                        </div>
+                        <div className="grid grid-cols-3 grid-rows-2 gap-2 aspect-square">
+                            <img
+                                className="rounded-xl col-span-3 object-cover h-full w-full place-items-stretch"
+                                src="/images/aerial_photo_moss.jpg"
+                                alt="Aerial view of Moss"
+                            />
+                            <img
+                                className="rounded-xl object-cover h-full w-full place-items-stretch"
+                                src="/images/helmetphoto.jpg"
+                                alt="Me in a flight helmet"
+                            />
+                            <img
+                                className="rounded-xl col-span-2 object-cover h-full w-full place-items-stretch"
+                                src="/images/rocket-launch.jpg"
+                                alt="Scenic view of a rocket launch|"
+                            />
+                        </div>
                     </div>
                 </div>
-                <div className="grid grid-cols-2">
-                    <div></div>
-                    <div>
-                        <h2>My skills</h2>
-                        <p></p>
+                <div className="container max-w-screen-2xl grid scroll-mt-20 p-5 md:p-20">
+                    <div className="grid gap-6">
+                        <div className="space-y-4">
+                            <h2 className="text-2xl font-semibold">
+                                Technologies I know / have used
+                            </h2>
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                                <TechCard
+                                    icon={<Globe />}
+                                    title="Web Development"
+                                >
+                                    <Tech
+                                        icon={<SiHtml5 color="#E34F26" />}
+                                        name="HTML"
+                                    />
+                                    <Tech
+                                        icon={<SiJavascript color="#F7DF1E" />}
+                                        name="Tailwind CSS"
+                                    />
+                                    <Tech
+                                        icon={<SiCss3 color="#1572B6" />}
+                                        name="CSS"
+                                    />
+                                    <Tech
+                                        icon={<SiSass color="#CC6699" />}
+                                        name="Sass"
+                                    />
+                                    <Tech
+                                        icon={<SiTailwindcss color="#06B6D4" />}
+                                        name="Tailwind CSS"
+                                    />
+                                </TechCard>
+
+                                <TechCard icon={<Code />} title="Programming">
+                                    <Tech
+                                        icon={<SiTypescript color="#3178C6" />}
+                                        name="TypeScript"
+                                    />
+                                    <Tech
+                                        icon={<SiRust color="#f04900" />}
+                                        name="Rust"
+                                    />
+                                    <Tech
+                                        icon={<SiZig color="#F7A41D" />}
+                                        name="Zig"
+                                    />
+                                    {/* TODO: Find icon with correct colors */}
+                                    <Tech
+                                        icon={<SiPython color="#3776AB" />}
+                                        name="Python"
+                                    />
+
+                                    <Tech
+                                        icon={<SiGo color="#00ADD8" />}
+                                        name="Go"
+                                    />
+                                    <Tech
+                                        icon={<SiDotnet color="#512BD4" />}
+                                        name=".NET (C#)"
+                                    />
+                                    <Tech
+                                        icon={<SiLua color="#2C2D72" />}
+                                        name="Lua"
+                                    />
+                                </TechCard>
+                                <TechCard icon={<Library />} title="Frameworks">
+                                    <Tech
+                                        icon={<SiReact color="#61DAFB" />}
+                                        name="React"
+                                    />
+                                    <Tech
+                                        icon={<SiVuedotjs color="#4FC08D" />}
+                                        name="Vue"
+                                    />
+                                    <Tech
+                                        icon={<SiSvelte color="#FF3E00" />}
+                                        name="Svelte"
+                                    />
+                                    <Tech
+                                        icon={<SiNextdotjs color="#FFFFFF" />}
+                                        name="Next.js"
+                                    />
+                                    {/* TODO: Replace this icon */}
+                                    {/* <Tech
+                                        icon={<SiVite color="#646CFF" />}
+                                        name="Vite"
+                                    /> */}
+                                    <Tech
+                                        icon={<SiRemix color="#FFFFFF" />}
+                                        name="Remix"
+                                    />
+                                </TechCard>
+
+                                <TechCard
+                                    icon={<Database />}
+                                    title="Databases"
+                                    className="flex flex-col gap-3"
+                                >
+                                    <div className="flex flex-auto gap-6 flex-wrap">
+                                        <Tech
+                                            icon={<SiMysql color="#4479A1" />}
+                                            name="MySQL"
+                                        />
+                                        <Tech
+                                            icon={
+                                                <SiPostgresql color="#4169E1" />
+                                            }
+                                            name="PostgreSQL"
+                                        />
+                                        <Tech
+                                            icon={
+                                                <SiSurrealdb color="#FF00A0" />
+                                            }
+                                            name="SurrealDB"
+                                        />
+                                        <Tech
+                                            icon={<SiMongodb color="#47A248" />}
+                                            name="MongoDB"
+                                        />
+                                    </div>
+                                    <Separator />
+                                    <div className="flex flex-auto gap-6 flex-wrap">
+                                        <Tech
+                                            icon={<SiDrizzle color="#C5F74F" />}
+                                            name="Drizzle"
+                                        />
+                                        <Tech
+                                            icon={<SiPrisma color="#2D3748" />}
+                                            name="Prisma"
+                                        />
+                                    </div>
+                                </TechCard>
+
+                                <TechCard
+                                    icon={<Smartphone />}
+                                    title="Mobile Development"
+                                >
+                                    <Tech
+                                        icon={<SiReact color="#61DAFB" />}
+                                        name="React Native"
+                                    />
+                                    <Tech
+                                        icon={<SiFlutter color="#02569B" />}
+                                        name="Flutter"
+                                    />
+                                    <Tech
+                                        icon={<SiAndroid color="#34A853" />}
+                                        name="Android"
+                                    />
+                                </TechCard>
+
+                                {/* 
+                                
+                                <TechItem
+                                    icon={<Smartphone className="h-5 w-5" />}
+                                    name="Mobile Dev"
+                                    techs={["React Native", "Flutter", "Swift"]}
+                                />
+                                
+                                */}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </>
+    );
+}
+
+function TechCard({
+    title,
+    icon,
+    className,
+    children,
+}: React.PropsWithChildren<{
+    title: string;
+    icon: React.ReactNode;
+    className?: string;
+}>) {
+    return (
+        <Card>
+            <CardHeader className="flex flex-row gap-3 font-medium">
+                {icon}
+                {title}
+            </CardHeader>
+            <CardContent
+                className={className ?? "flex flex-auto gap-6 flex-wrap"}
+            >
+                {children}
+            </CardContent>
+        </Card>
+    );
+}
+
+function Tech({ icon, name }: { icon: React.ReactNode; name: string }) {
+    return (
+        <div className="flex flex-row gap-3">
+            {icon}
+            <span className="text-muted-foreground">{name}</span>
+        </div>
     );
 }
